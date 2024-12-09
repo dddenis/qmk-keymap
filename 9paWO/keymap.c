5b23c51e7e4f437b184931288c5bb5304b5e262c
Denis Goncharenko <dddenjer@gmail.com>
2024-12-09 20:48:13 +0100

    feat: tmux prefix

--- 9paWO/keymap.c
+++ 9paWO/keymap.c
@@ -3,6 +3,9 @@
 #define MOON_LED_LEVEL LED_LEVEL
 #define ML_SAFE_RANGE SAFE_RANGE
 
+#define TMUX LT(0, KC_TAB)
+#define TMUX_KEY C(KC_A)
+
 enum custom_keycodes {
   RGB_SLD = ML_SAFE_RANGE,
 };
@@ -12,7 +15,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
+    TMUX,           KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     KC_ESCAPE,      MT(MOD_LGUI, KC_A),MT(MOD_LALT, KC_S),MT(MOD_LCTL, KC_D),MT(MOD_LSFT, KC_F),KC_G,                                           KC_H,           MT(MOD_RSFT, KC_J),MT(MOD_RCTL, KC_K),MT(MOD_LALT, KC_L),MT(MOD_RGUI, KC_SCLN),MT(MOD_RSFT, KC_QUOTE),
     KC_LEFT_GUI,    MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),KC_RIGHT_CTRL,  
                                                     LT(1,KC_SPACE), MT(MOD_LSFT, KC_ESCAPE),                                MT(MOD_RSFT, KC_ENTER),LT(2,KC_BSPC)
@@ -33,11 +36,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 };
 
-
-
+static bool tmux_enabled = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case TMUX:
+      if (record->tap.count == 0) {
+        tmux_enabled = record->event.pressed;
+        return false;
+      }
+      break;
 
     case RGB_SLD:
       if (record->event.pressed) {
@@ -45,6 +53,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
   }
+
+  if (tmux_enabled && record->event.pressed) {
+    tap_code16(TMUX_KEY);
+  }
+
   return true;
 }
 
